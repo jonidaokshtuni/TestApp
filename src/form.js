601daf3ac18a0f9ea0e21c2fe67a761d410377ac
Jonida Okshtuni <picari@forschner.com>
2023-08-02 21:15:30 +0200

    form styles finished

--- src/form.js
+++ src/form.js
@@ -3,12 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid';
 import { useForm, Controller, register } from 'react-hook-form';
 import "./form.css"
+import { FaArrowLeft } from 'react-icons/fa'
 
 const colors = ['red', 'green', 'blue', 'white', 'black'];
 
 const MyForm = ({ data, setData }) => {
-  console.log('ddd', data)
+  // console.log('ddd', data)
   const navigate = useNavigate()
+
   const {
     handleSubmit,
     control,
@@ -27,7 +29,7 @@ const MyForm = ({ data, setData }) => {
     //console.log('vvv',trueKeysArray)
     const uniqueId = uuidv4()
     dataAdd['contactPreference'] = trueKeysArray;
-    dataAdd['id']= uniqueId
+    dataAdd['id'] = uniqueId
     const newData = [...data, dataAdd];
     // localStorage.setItem('formData', JSON.stringify(newData));
     setData(newData)
@@ -35,106 +37,112 @@ const MyForm = ({ data, setData }) => {
     navigate('/')
   };
 
+  const handleGoBack = () => {
+    navigate('/')
+  };
+
+
   return (
     <div className="form-container">
-    <form id="myForm" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-group">
-        <label>Name:</label>
-        <Controller
-          name="name"
-          control={control}
-          rules={{ required: 'Name is required' }}
-          render={({ field }) => <input {...field} />}
-        />
-        {errors.name && <span>{errors.name.message}</span>}
-      </div>
-
-      <div className="form-group">
-        <label>Surname:</label>
-        <Controller
-          name="surname"
-          control={control}
-          rules={{ required: 'Surname is required' }}
-          render={({ field }) => <input {...field} />}
-        />
-        {errors.surname && <span>{errors.surname.message}</span>}
-      </div>
-
-      <div className="form-group">
-        <label>Email:</label>
-        <Controller
-          name="email"
-          control={control}
-          rules={{ required: 'Email is required', pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }}
-          render={({ field }) => <input {...field} />}
-        />
-        {errors.email && <span>{errors.email.message}</span>}
-      </div>
-
-      <div className="form-group">
-        <label>Age:</label>
-        <Controller
-          name="age"
-          control={control}
-          rules={{
-            required: 'Age is required',
-            min: { value: 0, message: 'Age must be a positive number' },
-            max: { value: 120, message: 'Age must be less than 120' }
-          }}
-          render={({ field }) => <input type="number" {...field} />}
-        />
-        {errors.age && <span>{errors.age.message}</span>}
-      </div>
-
-      <div className="form-group">
-        <label>Favorite color:</label>
-        <Controller
-          name="favoriteColor"
-          control={control}
-          rules={{ required: 'Favorite color is required' }}
-          render={({ field }) => (
-            <select {...field}>
-              <option value="">Select a color</option>
-              {colors.map((color) => (
-                <option key={color} value={color}>
-                  {color}
-                </option>
-              ))}
-            </select>
-          )}
-        />
-        {errors.favoriteColor && <span>{errors.favoriteColor.message}</span>}
-      </div>
-
-      <div className="form-group">
-        <label>Contact preference:</label>
-        {options.map((option) => (
-        <div key={option.id}>
+      <form id="myForm" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>Name:</label>
           <Controller
-           name={`options[${option.label}]`}
+            name="name"
             control={control}
-            defaultValue={false}
+            rules={{ required: 'Name is required' }}
+            render={({ field }) => <input {...field} />}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+
+        <div className="form-group">
+          <label>Surname:</label>
+          <Controller
+            name="surname"
+            control={control}
+            rules={{ required: 'Surname is required' }}
+            render={({ field }) => <input {...field} />}
+          />
+          {errors.surname && <span>{errors.surname.message}</span>}
+        </div>
+
+        <div className="form-group">
+          <label>Email:</label>
+          <Controller
+            name="email"
+            control={control}
+            rules={{ required: 'Email is required', pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }}
+            render={({ field }) => <input {...field} />}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+
+        <div className="form-group">
+          <label>Age:</label>
+          <Controller
+            name="age"
+            control={control}
+            rules={{
+              required: 'Age is required',
+              min: { value: 0, message: 'Age must be a positive number' },
+              max: { value: 120, message: 'Age must be less than 120' }
+            }}
+            render={({ field }) => <input type="number" {...field} />}
+          />
+          {errors.age && <span>{errors.age.message}</span>}
+        </div>
+
+        <div className="form-group">
+          <label>Favorite color:</label>
+          <Controller
+            name="favoriteColor"
+            control={control}
+            rules={{ required: 'Favorite color is required' }}
             render={({ field }) => (
-              <label>
-                <input
-                  type="checkbox"
-                  {...field}
-                  value={option.id}
-                  checked={field.value}
-                />
-                {option.label}
-              </label>
+              <select {...field}>
+                <option value="">Select a color</option>
+                {colors.map((color) => (
+                  <option key={color} value={color}>
+                    {color}
+                  </option>
+                ))}
+              </select>
             )}
           />
+          {errors.favoriteColor && <span>{errors.favoriteColor.message}</span>}
         </div>
-      ))}
-        {errors.contactPreference && <span>{errors.contactPreference.message}</span>}
-      </div>
-<div className="submit-div">
-<button type="submit">Submit</button>
-</div>
-    
-    </form>
+
+        <div className="form-group">
+          <label>Contact preference:</label>
+          {options.map((option) => (
+            <div key={option.id}>
+              <Controller
+                name={`options[${option.label}]`}
+                control={control}
+                defaultValue={false}
+                render={({ field }) => (
+                  <label>
+                    <input
+                      type="checkbox"
+                      {...field}
+                      value={option.id}
+                      checked={field.value}
+                    />
+                    {option.label}
+                  </label>
+                )}
+              />
+            </div>
+          ))}
+          {errors.contactPreference && <span>{errors.contactPreference.message}</span>}
+        </div>
+        <div className="submit-div">
+          <button className="go-back-button" onClick={handleGoBack}>  <FaArrowLeft className="icon" /> Go Back</button>
+          <button type="submit">Submit</button>
+        </div>
+
+      </form>
     </div>
   )
 }
